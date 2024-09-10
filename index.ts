74fa5c794bfd1cd78bf5176d79be790c34fd8d1e
ledhieus <ledan0602@gmail.com>
2024-09-10 08:41:29 +0700

    create project

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express()
+const port: number = 3000
+
+app.set("views", "./views")
+app.set("view engine", "pug")
+
+app.get("/tours", (req: Request, res: Response)=> {
+    res.render("clients/pages/tours/index")
+})
+
+app.listen(port, ()=> {
+    console.log(`App listening on port ${port}`)
+})
