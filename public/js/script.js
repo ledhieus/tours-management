9458fb8291253eb3ed8ce4535a0fe98474f71396
ledhieus <ledan0602@gmail.com>
2024-09-12 10:11:06 +0700

    thêm slide hình ảnh

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,18 @@
-console.log("OK")
+// slider Tour Detail
+var imagesThumb = new Swiper(".imagesThumb", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var imageMain = new Swiper(".imageMain", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: imagesThumb,
+    },
+  });
+// End slider Tour Detail
