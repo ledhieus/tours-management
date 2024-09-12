b2ee8f16676b78bc113fdfb031f6c874513af0c7
ledhieus <ledan0602@gmail.com>
2024-09-12 10:36:32 +0700

    thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,39 @@ var imagesThumb = new Swiper(".imagesThumb", {
     },
   });
 // End slider Tour Detail
+
+//carts
+//Nếu chưa có giỏ hàng thì tạo giỏ hàng
+const cart = localStorage.getItem("cart")
+if(!cart){
+  console.log("card")
+  localStorage.setItem("cart", JSON.stringify([]))
+}
+//Thêm tour vào cart
+const formAddToCart = document.querySelector("[form-add-to-cart]")
+if(formAddToCart){
+  formAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const quantity = parseInt(event.target.elements.quantity.value)
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"))
+    if(quantity > 0 && tourId){
+      const cart = JSON.parse(localStorage.getItem("cart"))
+
+      const indexExistTour = cart.findIndex(item => item.tourId == tourId)
+
+      if(indexExistTour == -1){
+        cart.push({
+          tourId: tourId,
+          quantity: quantity
+        })
+      } else {
+        cart[indexExistTour].quantity = cart[indexExistTour].quantity + quantity
+      }
+      
+
+      localStorage.setItem("cart", JSON.stringify(cart))
+    }
+  })
+}
+//end carts
